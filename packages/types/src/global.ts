6641f9c43820328c276211c97352bf59087702c9
navigaid <navigaid@gmail.com>
2022-03-14 16:19:51 +0800

    add nftmart submodule

--- packages/types/src/global.ts
+++ packages/types/src/global.ts
@@ -1,16 +1,31 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {ApiPromise} from '@polkadot/api';
+import {ApiPromise, Keyring} from '@polkadot/api';
 import {ApiDecoration} from '@polkadot/api/types';
 import Pino from 'pino';
 import {Store, DynamicDatasourceCreator} from './interfaces';
 
 type ApiAt = ApiDecoration<'promise'> & {rpc: ApiPromise['rpc']};
 
-declare global {
-  const api: ApiAt;
-  const logger: Pino.Logger;
-  const store: Store;
-  const createDynamicDatasource: DynamicDatasourceCreator;
+let store: Store;
+let api: ApiPromise;
+let patchedApi: ApiPromise;
+let logger: Pino.Logger;
+let keyring: Keyring;
+
+function setGlobal(vars: {
+  logger: Pino.Logger;
+  api: ApiPromise;
+  patchedApi: ApiPromise;
+  store: Store;
+  keyring: Keyring;
+}) {
+  store = vars.store;
+  api = vars.api;
+  patchedApi = vars.patchedApi;
+  logger = vars.logger;
+  keyring = vars.keyring;
 }
+
+export {store, api, patchedApi, logger, keyring, setGlobal};
